b9e38c8e5a430861790a1eb936b0a822b6467a15
alan barbera <alanbarbera02@gmail.com>
2025-01-19 12:43:07 -0300

    create: page login.html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const formularioContactos = document.querySelector("#formularioContactos");
+
+formularioContactos.addEventListener("submit", prueba);
+
+function prueba (e) {
+    e.preventDefault();
+
+    const datosFormulario = {
+        nombre: document.querySelector("#nombre-form").value,
+        email: document.querySelector("#email-form").value,
+        telefono: document.querySelector("#telefono-form").value,
+        mensaje: document.querySelector("#mensaje-form").value
+    }
+
+    sessionStorage.setItem("formulario", JSON.stringify(datosFormulario));
+};
+
+sessionStorage.clear(); 
